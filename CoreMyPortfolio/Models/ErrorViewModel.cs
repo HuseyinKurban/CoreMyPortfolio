19da13fdfad339990257843e787b64accbfe6504
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-11-16 21:35:17 +0300

    Add project files.

---
+++ CoreMyPortfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoreMyPortfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
