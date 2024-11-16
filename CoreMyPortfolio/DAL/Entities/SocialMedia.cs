d52b350e8b732b506b2781942b07e5efc911faca
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-11-16 22:15:13 +0300

    Add project files.

---
+++ CoreMyPortfolio/DAL/Entities/SocialMedia.cs
@@ -0,0 +1,10 @@
+﻿namespace CoreMyPortfolio.DAL.Entities
+{
+    public class SocialMedia
+    {
+        public int SocialMediaId { get; set; }
+        public string Title { get; set; }
+        public string İcon { get; set; }
+        public string Url { get; set; }
+    }
+}
