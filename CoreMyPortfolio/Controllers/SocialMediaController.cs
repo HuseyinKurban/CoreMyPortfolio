8031489c87a1b8c38705709e8a8a8cf83dc25009
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-11-20 02:52:31 +0300

    Add project files.

---
+++ CoreMyPortfolio/Controllers/SocialMediaController.cs
@@ -0,0 +1,54 @@
+﻿using CoreMyPortfolio.DAL.Context;
+using CoreMyPortfolio.DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMyPortfolio.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+
+        public IActionResult SocialMediaList()
+        {
+            var values = context.SocialMedias.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateSocialMedia()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateSocialMedia(SocialMedia socialmedia)
+        {
+            context.SocialMedias.Add(socialmedia);
+            context.SaveChanges();
+            return RedirectToAction("SocialMediaList");
+        }
+
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            var values = context.SocialMedias.Find(id);
+            context.SocialMedias.Remove(values);
+            context.SaveChanges();
+            return RedirectToAction("SocialMediaList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateSocialMedia(int id)
+        {
+            var values=context.SocialMedias.Find(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateSocialMedia(SocialMedia socialmedia)
+        {
+            context.SocialMedias.Update(socialmedia);
+            context.SaveChanges();
+            return RedirectToAction("SocialMediaList");
+        }
+    }
+}
---
+++ CoreMyPortfolio/Controllers/TestimonialsController.cs
@@ -0,0 +1,54 @@
+﻿using CoreMyPortfolio.DAL.Context;
+using CoreMyPortfolio.DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMyPortfolio.Controllers
+{
+    public class TestimonialsController : Controller
+    {
+        MyPortfolioContext context=new MyPortfolioContext();
+
+        public IActionResult TestimonialList()
+        {
+            var values=context.Testimonials.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateTestimonial()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateTestimonial(Testimonial testimonial)
+        {
+            context.Testimonials.Add(testimonial);
+            context.SaveChanges();
+            return RedirectToAction("TestimonialList");
+        }
+
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var values = context.Testimonials.Find(id);
+            context.Testimonials.Remove(values);
+            context.SaveChanges();
+            return RedirectToAction("TestimonialList");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateTestimonial(int id)
+        {
+            var values = context.Testimonials.Find(id);
+            return View(values);     
+        }
+
+        [HttpPost]
+        public IActionResult UpdateTestimonial(Testimonial testimonial)
+        {
+            context.Testimonials.Update(testimonial);
+            context.SaveChanges();
+            return RedirectToAction("TestimonialList");
+        }
+    }
+}
