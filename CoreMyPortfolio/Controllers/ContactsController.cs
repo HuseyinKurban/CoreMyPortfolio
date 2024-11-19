057bffde386156cd86ee0b31a966ae3647136394
Hüseyin Kurban <huseyinkurban03@gmail.com>
2024-11-19 23:45:52 +0300

    Add project files.

---
+++ CoreMyPortfolio/Controllers/ContactsController.cs
@@ -0,0 +1,34 @@
+﻿using CoreMyPortfolio.DAL.Context;
+using CoreMyPortfolio.DAL.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMyPortfolio.Controllers
+{
+    public class ContactsController : Controller
+    {
+        MyPortfolioContext context=new MyPortfolioContext();
+
+        public IActionResult ContactsList()
+        {
+            var values=context.Contacts.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult UpdateContacts(int id)
+        {
+            var values= context.Contacts.Find(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateContacts(Contact contact)
+        {
+           
+            context.Contacts.Update(contact);
+            context.SaveChanges();
+            return RedirectToAction("ContactsList");
+        }
+
+    }
+}
